cb48fbe3f58bfe051a5d23ee5ebf0cdfc2117790
KozmunkasKalman <kalmankozmunkas@gmail.com>
2025-10-30 21:17:58 +0100

    added gparu itself and an install script

---
+++ install.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+install() {
+  printf "Installing GParu...\n"
+  curl -fsSL "https://raw.githubusercontent.com/KozmunkasKalman/gparu/main/gparu" -o /tmp/gparu
+
+  if [ $? -ne 0 ]; then
+    echo "Error: Downloading of file failed."
+    exit 1
+  fi
+
+  chmod +x /tmp/gparu
+
+  echo "Installing gparu into /usr/local/bin/ ..."
+  sudo mv /tmp/gparu /usr/local/bin/gparu
+  if [ $? -ne 0 ]; then
+    echo "Error: Installation failed:\nUnable to move file from /tmp/ to /usr/local/bin/\nMost likely missing privileges or file system non-fhs compliant."
+  else
+    echo "GParu installed successfully. Run \"gparu help\" for more info."
+  fi
+}
+
+read -p "$(printf 'Are you sure you would like to install GParu? ')" action &&
+
+case "$action" in
+  Y|y|YES|Yes|yes) install;;
+  *) printf "Installation cancelled by user.\n"
+esac
+
+
