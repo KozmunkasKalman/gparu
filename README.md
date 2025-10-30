64b85c53bc9450e3fdb07e4139b91ffbcab519ad
KozmunkasKalman <kalmankozmunkas@gmail.com>
2025-10-30 21:27:36 +0100

    updated README.md and the install script

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# gparu
-TUI frontend for [Paru](https://github.com/Morganamilo/paru)
+# GParu
+
+TUI frontend for [Paru](https://github.com/Morganamilo/paru) AUR helper
+
+Usage:
+- gparu                                - interactive TUI with fzf
+- gparu {i install -i -S} [package(s)] - install package(s)
+- gparu {u update -u -Syu}             - update installed packages
+- gparu {r remove -r -R} [package(s)]  - remove installed package(s)
+- gparu {c clear-cache -c -Scc}        - clear cached files
+- gparu {h help -h}                    - prints help text
--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+set -e
+
 install() {
-  printf "Installing GParu...\n"
+  printf "Downloading gparu... (1/2)\n"
   curl -fsSL "https://raw.githubusercontent.com/KozmunkasKalman/gparu/main/gparu" -o /tmp/gparu
 
   if [ $? -ne 0 ]; then
@@ -11,7 +13,7 @@ install() {
 
   chmod +x /tmp/gparu
 
-  echo "Installing gparu into /usr/local/bin/ ..."
+  printf "Installing gparu into the /usr/local/bin/ directory... (2/2)\n"
   sudo mv /tmp/gparu /usr/local/bin/gparu
   if [ $? -ne 0 ]; then
     echo "Error: Installation failed:\nUnable to move file from /tmp/ to /usr/local/bin/\nMost likely missing privileges or file system non-fhs compliant."
